6f23f4c7705782e779d3ba4a9ca8ef7ffc0500ca
Lisa Cabrera <lisaannecabrera@gmail.com>
2020-02-16 20:19:59 -0800

    fixed delete so it will delete the right note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,7 +5,9 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = app => {
-  app.get("/api/notes", (req, res) => res.json(array));
+  app.get("/api/notes", (req, res) => {
+      return res.json(array);
+  });
   app.post("/api/notes", (req, res) => {
     array.push(req.body);
     for (let i = 0; i < array.length; i++) {
@@ -19,35 +21,28 @@ module.exports = app => {
         if (err) throw err;
       }
     );
-
-    app.delete("/api/notes/:id", (req, res) => {
-      const id = req.params.id;
-      let index;
-      for (let i in array) {
-        if (id === array[i].id) {
-          index = i;
-        }
-      }
-      array.splice(index, 1);
-      for (let i in array) {
-        array[i].id = i;
-      }
-      s.writeFile(
-        path.join(__dirname, "../db/db.json"),
-        JSON.stringify(array),
-        err => {
-          if (err) throw err;
-        }
-      );
-      return res.json(true);
-    });
+    return res.json(true);
   });
 
-  app.post("/api/clear", (req, res) => {
-    // Empty out the arrays of data
-    tableData.length = 0;
-    waitListData.length = 0;
-
-    res.json({ ok: true });
+  app.delete("/api/notes/:id", (req, res) => {
+    const id = req.params.id;
+    let index;
+    for (let i in array) {
+      if (id === array[i].id) {
+        index = i;
+      }
+    }
+    array.splice(index, 1);
+    for (let i in array) {
+      array[i].id = i;
+    }
+    fs.writeFile(
+      path.join(__dirname, "../db/db.json"),
+      JSON.stringify(array),
+      err => {
+        if (err) throw err;
+      }
+    );
+    return res.json(true);
   });
 };
